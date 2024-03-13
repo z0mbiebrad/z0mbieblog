feac7c75986b1d12370b702197a04a35791bcbfe
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2024-03-13 01:05:35 -0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,6 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/dashboard', function () {
