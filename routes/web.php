5583f7eef0ff2ed21c08585bd07da0eb586d47ef
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2024-03-13 01:04:40 -0400

    fix home page

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
