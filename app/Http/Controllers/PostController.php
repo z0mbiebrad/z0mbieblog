3ba74f9e5bead1a149e8745465b8ca1ca878b758
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2024-04-18 12:01:40 -0400

    sort by desc

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::all()->sortDesc();
         $images = Image::all();
 
         return view('posts.index', compact(['posts', 'images']));
