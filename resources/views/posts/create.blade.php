eed573d833eb84357b0c73ec303041ddfd9c2ead
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2024-04-29 11:39:06 -0400

    Update create.blade.php
    Moved images under create blog
    Made it so Submitting a blog post clears the stored text in body and title
    Moved the delete image button to the top so its easier to find

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,56 +6,18 @@
     @endif
 
     @if ($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li class="text-white p-2">{{ $error }}</li>
-                @endforeach
-            </ul>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li class="text-white p-2">{{ $error }}</li>
+            @endforeach
+        </ul>
     @endif
 
-    <div class="container mx-auto my-8 text-white">
-        <h1 class="text-3xl font-semibold text-white mb-6">Images:</h1>
-
-        <form action="{{ route('image.store') }}" method="post" enctype="multipart/form-data">
-            @csrf
-
-            <div class="mb-4 flex">
-                <input type="file" name="images[]" id="images" multiple
-                    accept="image/jpeg, image/png, image/jpg, image/gif, image/svg" class="mt-1 p-2" required>
-                <button type="submit" class="bg-gray-500 text-white px-4 rounded-md hover:bg-gray-600">Upload
-                    Images</button>
-            </div>
-        </form>
-
-        @if (!empty($images[0]))
-            <form action="{{ route('image.destroy') }}" method="post">
-                @csrf
-                @method('DELETE')
-
-                <div class="mb-4 flex flex-wrap">
-                    @foreach ($images as $image)
-                        <div class="basis-1/4 box-content flex relative p-2">
-                            <input type="checkbox" name="selected_images[]" id="image{{ $loop->iteration }}"
-                                value="{{ $image->id }}" class="absolute top-2 left-2"
-                                onclick="copyToClipboard('![image]({{ asset('storage/' . $image->image_path) }})')">
-
-                            <label for="image{{ $loop->iteration }}">
-                                <img src="{{ asset('storage/' . $image->image_path) }}" class="">
-                            </label>
-                        </div>
-                    @endforeach
-                </div>
-                <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">Delete
-                    Selected</button>
-            </form>
-        @endif
-    </div>
-
-    <div class="container mx-auto my-8 text-white">
+    <div class="container mx-auto my-4 text-white">
         <h1 class="text-3xl font-semibold text-white mb-6">Create a New Blog Post</h1>
 
         <div>
-            <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data">
+            <form action="{{ route('post.store') }}" method="post" enctype="multipart/form-data" id="post.store">
                 @csrf
                 <div class="mb-4">
                     <label for="title" class="block text-sm font-medium pt-6">Title:</label>
@@ -74,6 +36,45 @@
             </form>
         </div>
     </div>
+
+    <div class="container mx-auto my-4 text-white">
+        <h1 class="text-3xl font-semibold text-white mb-6">Images:</h1>
+
+        <form action="{{ route('image.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+
+            <div class="mb-4 flex">
+                <input type="file" name="images[]" id="images" multiple
+                    accept="image/jpeg, image/png, image/jpg, image/gif, image/svg" class="mt-1 p-2" required>
+                <button type="submit" class="bg-gray-500 text-white px-4 rounded-md hover:bg-gray-600">Upload
+                    Images</button>
+            </div>
+        </form>
+
+        @if (!empty($images[0]))
+            <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">Delete
+                Selected</button>
+            <form action="{{ route('image.destroy') }}" method="post">
+                @csrf
+                @method('DELETE')
+
+                <div class="mb-4 flex flex-wrap">
+                    @foreach ($images as $image)
+                        <div class="basis-1/4 box-content flex relative p-2">
+                            <input type="checkbox" name="selected_images[]" id="image{{ $loop->iteration }}"
+                                value="{{ $image->id }}" class="absolute top-2 left-2"
+                                onclick="copyToClipboard('![image]({{ asset('storage/' . $image->image_path) }})')">
+
+                            <label for="image{{ $loop->iteration }}">
+                                <img src="{{ asset('storage/' . $image->image_path) }}" class="">
+                            </label>
+                        </div>
+                    @endforeach
+                </div>
+
+            </form>
+        @endif
+    </div>
 </x-app-layout>
 
 <script>
@@ -87,21 +88,32 @@
     }
 
     document.addEventListener('DOMContentLoaded', function() {
-        var bodyTextarea = document.getElementById('title');
+        var titleTextarea = document.getElementById('title');
         var savedContent = localStorage.getItem('blogTitle');
-        bodyTextarea.value = savedContent;
-        bodyTextarea.addEventListener('input', function() {
+
+        titleTextarea.value = savedContent;
+        titleTextarea.addEventListener('input', function() {
             localStorage.setItem('blogTitle', this.value);
         });
     });
 
     document.addEventListener('DOMContentLoaded', function() {
-        var titleTextarea = document.getElementById('body');
+        var bodyTextarea = document.getElementById('body');
         var savedContent = localStorage.getItem('blogBody');
-        titleTextarea.value = savedContent;
-        titleTextarea.addEventListener('input', function() {
+
+        bodyTextarea.value = savedContent;
+        bodyTextarea.addEventListener('input', function() {
             localStorage.setItem('blogBody', this.value);
         });
     });
 
+    document.addEventListener('DOMContentLoaded', function() {
+        var postForm = document.getElementById('post.store');
+        postForm.addEventListener('submit', function() {
+            console.log('test');
+
+            localStorage.removeItem('blogTitle');
+            localStorage.removeItem('blogBody');
+        });
+    });
 </script>
