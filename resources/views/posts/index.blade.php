b7246ac500f030199450d352d0dbd9dd46d3eb81
z0mbiebrad <bradleyt.evans@gmail.com>
2024-10-24 20:04:46 -0400

    wip

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,7 +12,9 @@
             <p class="mt-2 text-lg leading-8 text-gray-300">Let's learn together!</p>
         </div>
         @foreach ($posts as $post)
-            <div class="mx-auto max-w-7xl lg:px-8">
+            <div class="mx-auto lg:px-8">
+                <div class="mx-auto max-w-2xl">
+                    <div class="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
                 @can('admin')
                     <div class="flex self-center justify-end space-x-4 mb-6">
                         <form action="{{ route('post.destroy', $post) }}" method="post">
@@ -29,15 +31,13 @@
                         </button>
                     </div>
                 @endcan
-                <div class="mx-auto max-w-2xl">
-                    <div class="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
                         <article class="flex max-w-xl flex-col items-start justify-between">
                             <div class="flex items-center gap-x-4 text-xs">
                                 <time datetime="2020-03-16"
                                     class="text-gray-300">{{ $post->created_at->format('Y-m-d') }}</time>
                             </div>
                             <div
-                                class="group relative prose prose-headings:text-gray-200 prose-p:text-gray-100 prose-a:text-blue-400">
+                                class="group relative prose prose-headings:text-gray-200 prose-p:text-gray-100 prose-a:text-blue-400 prose-p:break-all">
                                 <p class="mt-5 line-clamp-3 text-sm leading-6">
                                     {!! $post->html !!}
                                 </p>
